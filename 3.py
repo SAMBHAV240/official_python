483c918a868f18dd1071d7638c73a06c9a75b2ec
Sambhav <135624625+SAMBHAV240@users.noreply.github.com>
2024-06-24 00:51:06 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,17 @@
+"""n=int(input("enter the value of n:- "))
+for i in range(1, n+1):
+    print(" "* (n-i),end="")
+    print("*" * (2*i-1),end="")
+    print("")"""
+
+n=int(input("enter the value of n:- "))
+for i in range(1, n+1):
+    print(" "* (n-i),end="")
+    print("*" * (2*i-1),)
+
+    """n = int(input("enter the value of n:- "))
+for i in range(1, n+1):
+    print(" " * (n - i) + "*" * (2 * i - 1))"""
+    
+
+    
